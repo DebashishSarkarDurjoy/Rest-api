a33d1786ce9dc34bb99eb8b2439311ac360974d1
dsdjoy4@gmail.com <dsdjoy4@gmail.com>
2022-01-30 14:40:02 +1100

    consuming api: stack overflow

--- api_test.py
+++ api_test.py
@@ -3,4 +3,13 @@ import json
 
 response = requests.get("https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow")
 
-print(response.json())
+response = response.json()
+
+for value in response['items']:
+    if value['answer_count'] == 0:
+        print(value['title'])
+        print(value['link'])
+    else:
+        print("skipped")
+
+    print()
